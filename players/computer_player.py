fee9ca00bde64abd7ce5fa7cc5ff44ee2b1c71be
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-04-27 20:28:02 +0200

    feat: add sleep for computer moves

--- players/computer_player.py
+++ players/computer_player.py
@@ -1,7 +1,8 @@
+import random
+import time
 from custom.rules import Rules
 from players.player_types import PlayerType
 from players.player import Player
-import random
 
 class ComputerPlayer(Player):
     def __init__(self, rules: Rules) -> None:
@@ -9,6 +10,10 @@ class ComputerPlayer(Player):
         self.PLAYER_TYPE = PlayerType.COMPUTER
             
     def do_turn(self, coins: int) -> int:
+        # Give the computer time to think (visually)
+        delay = (random.random() + 1) * 2
+        time.sleep(delay)
+        
         should_take = (coins - int(self._rules.misere)) % (self._rules.min_take + self._rules.max_take)
         
         if should_take not in range(self._rules.min_take, self._rules.max_take + 1):
